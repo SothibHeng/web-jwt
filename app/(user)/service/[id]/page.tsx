2113efe2bf7f6e89f516ae24b4e4b6000b66a994
sangsokea <sangsokea109@gmail.com>
2024-03-28 09:12:06 +0700

    generate metadata in product detial

--- app/(user)/service/[id]/page.tsx
+++ app/(user)/service/[id]/page.tsx
@@ -1,22 +1,43 @@
 import CardComponent from "@/components/card/CardProductDetail";
+import { Metadata, ResolvingMetadata } from "next";
 
-type PropsParams = {
-	params: {
-		id: number;
-	};
-	searchParams: any;
+type Props = {
+	params: { id: string };
+	searchParams: { [key: string]: string | string[] | undefined };
 };
 
 const ENDPOINT = "https://fakestoreapi.com/products/";
 
-const getData = async (id: number) => {
+const getData = async (id: string) => {
 	const res = await fetch(`${ENDPOINT}${id}`);
 	const data = await res.json();
 	console.log(data);
 	return data;
 };
 
-export default async function Detail(props: PropsParams) {
+export async function generateMetadata(
+	{ params, searchParams }: Props,
+	parent: ResolvingMetadata
+): Promise<Metadata> {
+	// read route params
+	const id = params.id;
+
+	// fetch data
+	const product = await fetch(`https://fakestoreapi.com/products/${id}`).then((res) => res.json());
+
+	// optionally access and extend (rather than replace) parent metadata
+	const previousImages = (await parent).openGraph?.images || [];
+
+	return {
+		title: product.title,
+		description: product.description,
+		openGraph: {
+			images: [product.image, ...previousImages],
+		},
+	};
+}
+
+export default async function Detail(props: Props) {
 	let data = await getData(props.params.id);
 
 	return (
