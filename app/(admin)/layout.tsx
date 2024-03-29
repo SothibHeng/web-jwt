adea106539ee63db163000625e769432c1f62532
sangsokea <sangsokea109@gmail.com>
2024-03-29 10:12:59 +0700

    added datable to dashboard

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,17 +1,31 @@
+"use client";
 import SideBarComponent from "@/components/sidebar/SideBarComponent";
 import "@/app/globals.css";
+import { MenuIcon } from "@/components/icons/FontAwsome";
+import { useState } from "react";
+
 export default function AdminLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	const [isShowSideBar, setIsShowSideBar] = useState<boolean>(true);
+	console.log(isShowSideBar);
 	return (
 		<html>
-			<body>
-				<aside className="fixed h-screen">
+			<body className="flex none-scroll-bar">
+				<MenuIcon
+					onClick={() => setIsShowSideBar(!isShowSideBar)}
+					classname="h-8 z-20 w-8 fixed bottom-0 m-4 cursor-pointer lg:hidden"
+				/>
+				<aside
+					className={`sticky left-0 z-10 h-screen ${
+						isShowSideBar && "hidden"
+					} lg:block`}
+				>
 					<SideBarComponent />
 				</aside>
-				<main className="">{children}</main>
+				<main className="flex-1">{children}</main>
 			</body>
 		</html>
 	);
